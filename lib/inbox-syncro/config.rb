b1bd2b8634eb2b8e807cbdc9cdd01187f01f1741
Kelly <kelly@kellyredding.com>
2012-05-21 16:06:08 -0500

    reorganized the config classes to help keep things nice and readable
    * each config type class gets its own file
    * each are required in as needed

--- lib/inbox-syncro/config.rb
+++ lib/inbox-syncro/config.rb
@@ -1,85 +1,28 @@
 require 'ns-options'
-require 'ns-options/boolean'
+require 'inbox-syncro/config/imap_config'
+require 'inbox-syncro/config/smtp_config'
 
-module InboxSyncro; end
+module InboxSyncro
 
-class InboxSyncro::Config
-  include NsOptions::Proxy
-
-  class Credentials
+  class Config
     include NsOptions::Proxy
 
-    opt :user, :required => true
-    opt :pw, :required => true
+    opt :source, IMAPConfig, :required => true, :default => {}
+    opt :dest,   IMAPConfig, :required => true, :default => {}
+    opt :notify, SMTPConfig, :required => true, :default => {}
 
-    def initialize(*args)
-      if args.size == 1
-        self.apply(args.last)
-      else
-        self.user, self.pw = args
-      end
-    end
-
-    def validate!
-      if !required_set?
-        raise ArgumentError, "some required configs are missing"
-      end
-    end
-  end
-
-  class IMAPConfig
-    include NsOptions::Proxy
-
-    opt :host, :required => true
-    opt :port, :default => 143, :required => true
-    opt :ssl, NsOptions::Boolean, :default => false, :required => true
-    opt :login, Credentials, :required => true, :default => {}
-    opt :inbox, :default => "INBOX", :required => true
-    opt :expunge, NsOptions::Boolean, :default => true, :required => true
+    opt :archive_folder, :default => 'Forwarded', :required => true
 
     def validate!
       if !required_set?
         raise ArgumentError, "some required configs are missing"
       end
 
-      login.validate!
-    end
-  end
-
-  class SMTPConfig
-    include NsOptions::Proxy
-
-    opt :host, :required => true
-    opt :port, :default => 587, :required => true
-    opt :tls,  NsOptions::Boolean, :default => true, :required => true
-    opt :helo, :required => true
-    opt :login, Credentials, :required => true, :default => {}
-    opt :authtype, :default => :login, :required => true
-    opt :to_addrs, :required => true
-
-    def validate!
-      if !required_set?
-        raise ArgumentError, "some required configs are missing"
-      end
-
-      login.validate!
-    end
-  end
-
-  opt :source, IMAPConfig, :required => true, :default => {}
-  opt :dest,   IMAPConfig, :required => true, :default => {}
-  opt :notify, SMTPConfig, :required => true, :default => {}
-
-  opt :archive_folder, :default => 'Forwarded', :required => true
-
-  def validate!
-    if !required_set?
-      raise ArgumentError, "some required configs are missing"
+      source.validate!
+      dest.validate!
+      notify.validate!
     end
 
-    source.validate!
-    dest.validate!
-    notify.validate!
   end
 
 end
